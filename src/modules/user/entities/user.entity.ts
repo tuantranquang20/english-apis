efe89b9e7a5ff5c2e9130daa9f668733e976b307
trunghoang <trunght@nal.vn>
2023-07-20 18:14:51 +0700

    them schema

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import * as bcrypt from 'bcryptjs';
+import { CollectionName } from '@src/commons/constants';
+import { BaseSchema } from 'src/commons/schema/base.schema';
+@Schema({ timestamps: true, collection: CollectionName.USERS })
+export class User extends BaseSchema {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true, unique: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: String, enum: ['user', 'admin'], default: 'user' })
+  role: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+// Hàm middleware để mã hóa mật khẩu trước khi lưu vào MongoDB
+UserSchema.pre<User>('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
